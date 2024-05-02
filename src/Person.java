61f791cdcb2abb09d99581036fd0762a43da1fae
Al Akib <alakibrahman@gmail.com>
2024-05-02 18:19:08 +0600

    added a assignment

---
+++ src/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+    private string name;
+    private int age;
+
+    public Person(string name,int age) {
+        name="Al Akib";
+        age=21;
+    }
+
+    public string getName() {
+        return name;
+    }
+
+    public void setName(string name) {
+        this.name=name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age=age;
+    }
+}
